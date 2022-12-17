e8ab2e7063f52878c95f613e586efbb99fb16f9e
Marcus Sullivan <sullivanml299@gmail.com>
2022-12-16 19:58:40 -0500

    made puzzle pieces draggable using OS package, but package is not in git. need to fix.

--- goblin_vault/lib/picture_puzzle.dart
+++ goblin_vault/lib/picture_puzzle.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_draggable_gridview/flutter_draggable_gridview.dart';
 
 class PicturePuzzle extends StatefulWidget {
   const PicturePuzzle({super.key, required this.validator});
@@ -38,57 +39,102 @@ class _PicturePuzzleState extends State<PicturePuzzle> {
         child: Center(child: gridBuilder()));
   }
 
-  GridView gridBuilder() {
-    return GridView.builder(
+  Widget gridBuilder() {
+    return DraggableGridViewBuilder(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 3,
-        crossAxisSpacing: 10,
         mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        // childAspectRatio: MediaQuery.of(context).size.width /
+        //     (MediaQuery.of(context).size.height / 3),
       ),
       shrinkWrap: true,
-      itemCount: pieces.length,
-      itemBuilder: (context, index) {
-        return makePiece(index);
+      children: listBuilder(),
+      isOnlyLongPress: false,
+      dragCompletion:
+          (List<DraggableGridItem> list, int beforeIndex, int afterIndex) {
+        swap(beforeIndex, afterIndex);
+      },
+      dragFeedback: (List<DraggableGridItem> list, int index) {
+        return Container(
+          width: 200,
+          height: 150,
+          child: list[index].child,
+        );
+      },
+      dragPlaceHolder: (List<DraggableGridItem> list, int index) {
+        return PlaceHolderWidget(
+          child: Container(
+            color: Colors.white,
+          ),
+        );
       },
     );
+
+    // return GridView.builder(
+    //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+    //     crossAxisCount: 3,
+    //     crossAxisSpacing: 10,
+    //     mainAxisSpacing: 10,
+    //   ),
+    //   shrinkWrap: true,
+    //   itemCount: pieces.length,
+    //   itemBuilder: (context, index) {
+    //     return makePiece(index);
+    //   },
+    // );
   }
 
-  Widget makePiece(int gridIndex) {
+  List<DraggableGridItem> listBuilder() {
+    List<DraggableGridItem> puzzlePieces = [];
+    for (var i = 0; i < pieces.length; i++) {
+      puzzlePieces.add(makePiece(i));
+    }
+    return puzzlePieces;
+  }
+
+  DraggableGridItem makePiece(int gridIndex) {
     PuzzlePiece piece = pieces[gridIndex];
-    return Material(
-        color: Colors.transparent,
-        child: Ink(
-          decoration: BoxDecoration(
-              border: selected != gridIndex
-                  ? null
-                  : Border.all(color: Colors.green, width: 5),
-              image: DecorationImage(image: piece.image!)),
-          child: InkWell(
-            splashColor: Colors.green,
-            onTap: () {
-              if (selected == null) {
-                setState(() {
-                  selected = gridIndex;
-                });
-              } else if (selected == gridIndex) {
-                setState(() {
-                  selected = null;
-                });
-              } else {
-                swap(selected!, gridIndex);
-              }
-            },
-          ),
+    return DraggableGridItem(
+        isDraggable: true,
+        child: Image(
+          image: piece.image!,
         ));
   }
+
+  // Widget makePiece(int gridIndex) {
+  //   PuzzlePiece piece = pieces[gridIndex];
+  //   return Material(
+  //       color: Colors.transparent,
+  //       child: Ink(
+  //         decoration: BoxDecoration(
+  //             border: selected != gridIndex
+  //                 ? null
+  //                 : Border.all(color: Colors.green, width: 5),
+  //             image: DecorationImage(image: piece.image!)),
+  //         child: InkWell(
+  //           splashColor: Colors.green,
+  //           onTap: () {
+  //             if (selected == null) {
+  //               setState(() {
+  //                 selected = gridIndex;
+  //               });
+  //             } else if (selected == gridIndex) {
+  //               setState(() {
+  //                 selected = null;
+  //               });
+  //             } else {
+  //               swap(selected!, gridIndex);
+  //             }
+  //           },
+  //         ),
+  //       ));
+  // }
 
   swap(int idx1, int idx2) {
     var temp = pieces[idx1];
     pieces[idx1] = pieces[idx2];
     pieces[idx2] = temp;
-    setState(() {
-      selected = null;
-    });
     checkSolved();
   }
 
